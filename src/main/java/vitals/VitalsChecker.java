75fd46ff34bb0095da59156d961dfc15eb68844f
Ishrath <ishrathfatima.ishrathfatima@philips.com>
2026-06-05 12:04:10 +0530

    Reduce CCN via rule-driven vital evaluation

--- src/main/java/vitals/VitalsChecker.java
+++ src/main/java/vitals/VitalsChecker.java
@@ -1,14 +1,25 @@
 package vitals;
 
+import java.io.PrintStream;
 
-public abstract class VitalsChecker {
+
+public final class VitalsChecker {
   private static final float MIN_TEMPERATURE = 95f;
   private static final float MAX_TEMPERATURE = 102f;
   private static final float MIN_PULSE_RATE = 60f;
   private static final float MAX_PULSE_RATE = 100f;
   private static final float MIN_SPO2 = 90f;
+  private static final float MAX_SPO2 = 100f;
   private static final int DEFAULT_ALERT_BLINKS = 6;
   private static final long DEFAULT_ALERT_DELAY_MILLIS = 1000;
+  private static final PrintStream DEFAULT_ALERT_OUTPUT = System.out;
+  private static final VitalRule[] VITAL_RULES = new VitalRule[] {
+      new VitalRule(MIN_TEMPERATURE, MAX_TEMPERATURE, "Temperature is critical!"),
+      new VitalRule(MIN_PULSE_RATE, MAX_PULSE_RATE, "Pulse Rate is out of range!"),
+      new VitalRule(MIN_SPO2, MAX_SPO2, "Oxygen Saturation out of range!")};
+
+  private VitalsChecker() {
+  }
 
   static boolean vitalsOk(float temperature, float pulseRate, float spo2) 
       throws InterruptedException {
@@ -30,14 +41,12 @@ public abstract class VitalsChecker {
   }
 
   static String firstCriticalVitalMessage(float temperature, float pulseRate, float spo2) {
-    if (isOutOfRange(temperature, MIN_TEMPERATURE, MAX_TEMPERATURE)) {
-      return "Temperature is critical!";
-    }
-    if (isOutOfRange(pulseRate, MIN_PULSE_RATE, MAX_PULSE_RATE)) {
-      return "Pulse Rate is out of range!";
-    }
-    if (spo2 < MIN_SPO2) {
-      return "Oxygen Saturation out of range!";
+    float[] vitalReadings = new float[] {temperature, pulseRate, spo2};
+    for (int i = 0; i < VITAL_RULES.length; i++) {
+      String outOfRangeMessage = VITAL_RULES[i].outOfRangeMessageFor(vitalReadings[i]);
+      if (outOfRangeMessage != null) {
+        return outOfRangeMessage;
+      }
     }
     return null;
   }
@@ -48,12 +57,31 @@ public abstract class VitalsChecker {
 
   private static void triggerAlert(String alertMessage, int blinkCount, long delayMillis)
       throws InterruptedException {
-    System.out.println(alertMessage);
+    DEFAULT_ALERT_OUTPUT.println(alertMessage);
     for (int i = 0; i < blinkCount; i++) {
-      System.out.print("\r* ");
+      DEFAULT_ALERT_OUTPUT.print("\r* ");
       Thread.sleep(delayMillis);
-      System.out.print("\r *");
+      DEFAULT_ALERT_OUTPUT.print("\r *");
       Thread.sleep(delayMillis);
+    }
+  }
+
+  private static final class VitalRule {
+    private final float min;
+    private final float max;
+    private final String message;
+
+    private VitalRule(float min, float max, String message) {
+      this.min = min;
+      this.max = max;
+      this.message = message;
+    }
+
+    private String outOfRangeMessageFor(float value) {
+      if (isOutOfRange(value, min, max)) {
+        return message;
+      }
+      return null;
     }
   }
 }
